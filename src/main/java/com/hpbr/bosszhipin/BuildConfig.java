4edb5810677fd952d89735ce3d21c185284bf133
liliangxing <253254457@qq.com>
2026-08-07 20:41:05 +0800

    Add BuildConfig.java (APPLICATION_ID, version info)

---
+++ src/main/java/com/hpbr/bosszhipin/BuildConfig.java
@@ -0,0 +1,21 @@
+package com.hpbr.bosszhipin;
+
+/* JADX INFO: loaded from: classes3.dex */
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.hpbr.bosszhipin";
+    public static final String APP_BRANCH_NAME = "dev_14.13";
+    public static final String APP_SHOW_VERSION_NAME = "14.130";
+    public static final String APP_VERSION_NAME = "14.130";
+    public static final String BUILD_NUMBER = "835";
+    public static final String BUILD_TIME = "2026-07-24 14:58:50";
+    public static final String BUILD_TYPE = "release";
+    public static final String CHANNEL_STRING = "0";
+    public static final String COMMIT_ID = "1f0ab0d2d09de2289f43aedc689abfd86b321989";
+    public static final int CONFIG = 1;
+    public static final boolean DEBUG = false;
+    public static final String FLAVOR = "prod";
+    public static final String GIT_BRANCH = "origin/master";
+    public static final boolean PRINT_LOG = false;
+    public static final int VERSION_CODE = 1413010;
+    public static final String VERSION_NAME = "14.130";
+}
